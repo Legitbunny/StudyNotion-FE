1c737966b58633fc852ee820141cd5f7a6cf3fb7
Legitbunny <shreyashsingh18feb@gmail.com>
2024-04-10 02:58:16 +0530

    resolved link error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from "react-icons/fa";
 const Home = () => {
   return (
     <div>
-        {/* section 1 */}
+        {/* section 1
         <div>
 
             <Link to= {"/signup"}>
@@ -16,7 +16,7 @@ const Home = () => {
                     </div>
                 </div>
             </Link>
-        </div>
+        </div> */}
     </div>
   )
 }
